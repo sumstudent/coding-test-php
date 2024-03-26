27165ebdc379360ca3faa5c2356a5c938b89e05d
sumstudent <deo.sumatra@yahoo.com>
2024-03-27 00:34:46 +0800

    Modified the view single article

--- app/templates/Articles/index.php
+++ app/templates/Articles/index.php
@@ -8,7 +8,7 @@
     <?php foreach ($articles as $article) : ?>
         <tr>
             <td>
-                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->id, '_ext' => 'json']) ?>
             </td>
             <td>
                 <?= isset($article->created_at) ? $article['created_at'] : 'N/A' ?>
