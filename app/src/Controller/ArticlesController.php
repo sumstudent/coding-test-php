647309d310f855196d8bc3a12cba1cc1dd360a9b
sumstudent <deo.sumatra@yahoo.com>
2024-03-27 01:43:08 +0800

    Modified to return 200 status code if success

--- app/src/Controller/ArticlesController.php
+++ app/src/Controller/ArticlesController.php
@@ -7,18 +7,23 @@ use App\Controller\AppController;
 
 class ArticlesController extends AppController
 {
+
+    public function beforeFilter(\Cake\Event\EventInterface $event) {
+
+        parent::beforeFilter($event);
+        $this->Authentication->addUnauthenticatedActions(['index','view']);
+    }
+
     public function index()
     {
         $articles = $this->Articles->find()->toArray(); // Fetches all articles as an array
-        $this->set(compact('articles'));
-        $this->viewBuilder()->setOption('serialize', ['articles']); //into the route for all articles
+        return $this->jsonResponse(200, $articles);
     }
 
     public function view($id)
     {
         $article = $this->Articles->get($id); //Fetches the article by id
-        $this->set(compact('article'));
-        $this->viewBuilder()->setOption('serialize', ['article']); //into the route for single article
+        return $this->jsonResponse(200, $article);
     }
 
     public function add()
