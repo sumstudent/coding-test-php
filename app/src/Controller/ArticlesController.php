e3d571d56c746fd71b6e7dc1bcce08f62f147af2
sumstudent <deo.sumatra@yahoo.com>
2024-03-27 01:05:56 +0800

    Added the add article feature

--- app/src/Controller/ArticlesController.php
+++ app/src/Controller/ArticlesController.php
@@ -14,9 +14,28 @@ class ArticlesController extends AppController
         $this->viewBuilder()->setOption('serialize', ['articles']); //into the route for all articles
     }
 
-    public function view($id){
+    public function view($id)
+    {
         $article = $this->Articles->get($id); //Fetches the article by id
         $this->set(compact('article'));
         $this->viewBuilder()->setOption('serialize', ['article']); //into the route for single article
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            $article->user_id = $this->Auth->user('id'); // Assign the current user's ID
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('The article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add the article. Please, try again.'));
+        }
+
+        $this->set(compact('article'));
     }
 }
